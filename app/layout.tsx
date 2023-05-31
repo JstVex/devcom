9310b70c9cbe3f65ae3c5a4637be381d34dae2eb
JstVex <heinhtetlulu14@gmail.com>
2023-06-01 02:08:46 +0630

    Create home page and layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,8 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Sidebar from '@/components/Sidebar'
+import clsx from 'clsx'
+import styles from '@/styles/Beam.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,9 +17,19 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        {children}
+    <html lang="en" className={inter.className}>
+      <body className='bg-slate-50 dark:bg-[#101931] h-screen text-zinc-900 dark:text-zinc-50  dark:bg-gradient-to-b from-[#0c1120] p-6'>
+        <div
+          className={clsx(
+            'absolute top-0 inset-x-0 bg-top bg-no-repeat',
+            styles[`beams-1`],
+          )}
+        />
+        <div className='fixed flex w-screen'>
+          <Sidebar />
+          {children}
+        </div>
+
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <main>
-      devcom starts now letsgooo
+    <main className="flex-grow ml-6 mr-12 min-h-[94vh] shadow-xl flex bg-slate-800 sm:rounded-xl dark:bg-slate-900/70 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10">
+      <div >
+        hiiiii
+      </div>
+
     </main>
   )
 }
