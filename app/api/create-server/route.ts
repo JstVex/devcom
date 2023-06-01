6313b69b05fba83eac9855b91148fb0ce27bef67
JstVex <heinhtetlulu14@gmail.com>
2023-06-01 23:46:59 +0630

    Create POST route to create server

---
+++ app/api/create-server/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import { db } from "@/lib/prisma";
+import getCurrentUser from "@/actions/getCurrentUser";
+
+export async function POST(
+    request: NextRequest
+) {
+    try {
+        const currentUser = await getCurrentUser();
+
+        if (!currentUser?.name || !currentUser?.id) {
+            return new NextResponse('Unauthorized', { status: 401 })
+        }
+
+        const body = await request.json();
+        const { name, description, maxMembers, status } = body;
+
+        if (!name || !description || !status) {
+            return new NextResponse('Missing credentials', { status: 400 });
+        }
+
+        const server = await db.server.create({
+            data: {
+                name,
+                owner: 'Julian',
+                ownerId: 'plsletmejust',
+                description,
+                maxMembers,
+                status
+            }
+        })
+
+        return NextResponse.json(server);
+    } catch (error: any) {
+        console.log('CREATE SERVER ERROR:', error);
+        return new NextResponse('Internal Error', { status: 500 });
+    }
+}
