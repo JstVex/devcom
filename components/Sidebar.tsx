8621eef428f4ab0a33ebd6d16b9a112768caa48b
JstVex <heinhtetlulu14@gmail.com>
2023-06-01 23:50:47 +0630

    Add owned and joined servers to sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,4 @@
-'use client'
+
 
 import { FC } from 'react'
 import Profile from './Profile'
@@ -7,33 +7,59 @@ import styles from '@/styles/Beam.module.css'
 import Link from 'next/link'
 import { Plus } from 'lucide-react'
 import { usePathname } from 'next/navigation';
+import OwnedServers from './OwnedServers'
+import JoinedServers from './JoinedServers'
+import getOwnedServers from '@/actions/getOwnedServers'
+import getJoinedServers from '@/actions/getJoinedServers'
 
-interface SidebarProps {
+// interface SidebarProps {
 
-}
+// }
 
-const Sidebar: FC<SidebarProps> = ({ }) => {
-    const pathname = usePathname();
+const Sidebar = async ({ }) => {
+    // const pathname = usePathname();
+    const ownedServers = await getOwnedServers();
+    const joinedServers = await getJoinedServers();
+
     return (
-        <div className='w-60'>
-            <Profile />
-            <div className='p-3 min-h-[85vh] mt-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/90 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10'>
-                {pathname !== '/' &&
+        <>
+            {/* {pathname !== '/signin' && */}
+            <div className='w-72'>
+                <Profile />
+                <div className='p-3 min-h-[85vh] mt-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/90 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10'>
+                    {/* {pathname !== '/' && pathname !== '/create-server' && */}
                     <Link href='/' >
                         <div className='text-sm py-2 px-3 flex items-center gap-x-2 rounded-md border cursor-pointer mb-3 dark:border-white/10'>
                             <Plus />
                             Create or join a new server
                         </div>
                     </Link>
-                }
+                    {/* } */}
 
-                <div className=''>
-                    Current servers
-                    {/* all joined servers */}
+                    <div className=''>
+                        <h3>
+                            Your servers
+                        </h3>
+                        {ownedServers.map((ownedServer) => {
+                            return <OwnedServers key={ownedServer.id} server={ownedServer} />
+                        })}
+
+                    </div>
+
+                    <div className=''>
+                        <h3>
+                            Joined servers
+                        </h3>
+                        {joinedServers.map((joinedServer) => {
+                            return <JoinedServers key={joinedServer.id} server={joinedServer} />
+                        })}
+                    </div>
                 </div>
-            </div>
 
-        </div>
+            </div>
+            {/* } */}
+
+        </>
 
     )
 }
