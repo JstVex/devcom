7cb8f79b73e6960b6cc4b5f52db7d214927f4143
JstVex <heinhtetlulu14@gmail.com>
2023-06-01 23:42:36 +0630

    Add user's name and mail using useSession

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -2,8 +2,9 @@
 
 import Image from 'next/image'
 import { FC, useState } from 'react'
-import { signIn } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
 import { Settings } from 'lucide-react'
+import { useSession } from 'next-auth/react';
 
 interface ProfileProps {
 
@@ -12,32 +13,33 @@ interface ProfileProps {
 const Profile: FC<ProfileProps> = ({ }) => {
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
-    const signInWithGithub = async () => {
-        setIsLoading(true)
+    const { data: session } = useSession();
 
-        try {
-            await signIn('github', {
-                redirect: false
-            })
-                .then((callback) => {
-                    if (callback?.error) {
-                        console.log('error is', callback.error)
-                    }
+    // const signOut = async () => {
+    //     setIsLoading(true)
 
-                    if (callback?.ok && !callback?.error) {
-                        console.log('success')
-                    }
-                })
-                .finally(() => setIsLoading(false));
-        }
-        catch (error) {
-            console.log(error)
-        }
-    }
+    //     try {
+    //         await signOut()
+    //             .then((callback) => {
+    //                 if (callback?.error) {
+    //                     console.log('error is', callback.error)
+    //                 }
+
+    //                 if (callback?.ok && !callback?.error) {
+    //                     console.log('success')
+    //                 }
+    //             })
+    //             .finally(() => setIsLoading(false));
+    //     }
+    //     catch (error) {
+    //         console.log(error)
+    //     }
+    // }
+
     return (
         <div className='py-2 px-3 shadow-xl h-auto flex items-center bg-slate-800 sm:rounded-xl dark:bg-slate-900/70 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 gap-x-3'>
             <Image
-                src={'/placeholder.jpeg'}
+                src={session?.user.image || '/placeholder.jpeg'}
                 alt='profile'
                 width={35}
                 height={35}
@@ -46,13 +48,19 @@ const Profile: FC<ProfileProps> = ({ }) => {
             />
             <div >
                 <div className='text-sm text-zinc-900 dark:text-zinc-50 '>
-                    Jst Vex
+                    {session?.user?.name ?
+                        session.user.name :
+                        'Unknown'
+                    }
                 </div>
                 <div className='text-xs text-zinc-400 dark:text-slate-300'>
-                    github acc
+                    {session?.user?.email ?
+                        session.user.email :
+                        '@gmail.com'
+                    }
                 </div>
             </div>
-            <Settings onClick={signInWithGithub} className='ml-auto text-zinc-900 dark:text-slate-300' />
+            <Settings onClick={() => signOut()} className='ml-auto text-zinc-900 dark:text-slate-300' />
         </div>
     )
 }
