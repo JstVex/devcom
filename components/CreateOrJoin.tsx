9a54802a1c5ae0fc88ec64fd077e665d7fbd16d0
JstVex <heinhtetlulu14@gmail.com>
2023-06-02 23:42:32 +0630

    Put create or join button inside its own client component

---
+++ components/CreateOrJoin.tsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { FC } from 'react'
+import { Plus } from 'lucide-react'
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+
+interface CreateOrJoinProps {
+
+}
+
+const CreateOrJoin: FC<CreateOrJoinProps> = ({ }) => {
+    const pathname = usePathname();
+    return (
+        <div>
+            {pathname !== '/' && pathname !== '/create-server' &&
+                <Link href='/' >
+                    <div className='text-sm py-2 px-3 flex items-center gap-x-2 rounded-md border cursor-pointer mb-3 dark:border-white/10'>
+                        <Plus />
+                        Create or join a new server
+                    </div>
+                </Link>
+            }
+        </div>
+    )
+}
+
+export default CreateOrJoin
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,16 +1,13 @@
-
-
 import { FC } from 'react'
 import Profile from './Profile'
 import clsx from 'clsx'
 import styles from '@/styles/Beam.module.css'
-import Link from 'next/link'
-import { Plus } from 'lucide-react'
 import { usePathname } from 'next/navigation';
 import OwnedServers from './OwnedServers'
 import JoinedServers from './JoinedServers'
 import getOwnedServers from '@/actions/getOwnedServers'
 import getJoinedServers from '@/actions/getJoinedServers'
+import CreateOrJoin from './CreateOrJoin'
 
 // interface SidebarProps {
 
@@ -26,16 +23,8 @@ const Sidebar = async ({ }) => {
             {/* {pathname !== '/signin' && */}
             <div className='w-72'>
                 <Profile />
-                <div className='p-3 min-h-[85vh] mt-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/90 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10'>
-                    {/* {pathname !== '/' && pathname !== '/create-server' && */}
-                    <Link href='/' >
-                        <div className='text-sm py-2 px-3 flex items-center gap-x-2 rounded-md border cursor-pointer mb-3 dark:border-white/10'>
-                            <Plus />
-                            Create or join a new server
-                        </div>
-                    </Link>
-                    {/* } */}
-
+                <div className='p-3 overflow-y-auto min-h-[84vh] max-h-[84vh] mt-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/90 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10'>
+                    <CreateOrJoin />
                     <div className=''>
                         <h3>
                             Your servers
