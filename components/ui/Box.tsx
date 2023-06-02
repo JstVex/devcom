2a48499741b6bfff86fe4796091d99d6696c5209
JstVex <heinhtetlulu14@gmail.com>
2023-06-02 23:42:47 +0630

    Add max height for overflow

--- components/ui/Box.tsx
+++ components/ui/Box.tsx
@@ -8,7 +8,7 @@ interface BoxProps {
 
 const Box: FC<BoxProps> = ({ children, className }) => {
     return (
-        <div className={clsx("flex-grow overflow-y-auto ml-6 mr-12 min-h-[94vh] shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/70 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10", className)}>
+        <div className={clsx("flex-grow overflow-y-auto ml-6 mr-12 min-h-[94vh] max-h-[94vh] shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/70 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10", className)}>
             {children}
         </div>
     )
