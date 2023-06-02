7df26494000ffe405388031c74c7cb95ae655852
JstVex <heinhtetlulu14@gmail.com>
2023-06-02 23:41:57 +0630

    Link to each server on click

--- components/JoinedServers.tsx
+++ components/JoinedServers.tsx
@@ -1,5 +1,6 @@
 import { FC } from 'react'
 import { Server } from '@prisma/client'
+import Link from 'next/link'
 
 interface JoinedServersProps {
     server: Server
@@ -7,16 +8,18 @@ interface JoinedServersProps {
 
 const OwnedServers: FC<JoinedServersProps> = ({ server }) => {
     return (
-        <div className='text-slate-900 w-full rounded-md px-4 py-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/10 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 dark:text-slate-100'>
-            <div>
-                <h4 className='text-sm'>
-                    {server.name}
-                </h4>
-                <p className='text-xs font-light text-slate-500 dark:text-slate-400'>
-                    {server.description}
-                </p>
+        <Link href={`/${server.id}`}>
+            <div className='text-slate-900 w-full rounded-md my-2 px-4 py-3 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/10 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 dark:text-slate-100'>
+                <div>
+                    <h4 className='text-sm'>
+                        {server.name}
+                    </h4>
+                    <p className='text-xs font-light text-slate-500 dark:text-slate-400'>
+                        {server.description}
+                    </p>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
