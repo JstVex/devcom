4dd5d8e5707b64c462ad44ceb12e209859488a52
JstVex <heinhtetlulu14@gmail.com>
2023-06-07 22:24:32 +0630

    Add the user to the user list when join

--- components/home/RecommendedServers.tsx
+++ components/home/RecommendedServers.tsx
@@ -1,5 +1,9 @@
+'use client'
+
 import { Server } from '@prisma/client'
+import axios from 'axios'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
 import { FC } from 'react'
 
 interface RecommendedServersProps {
@@ -7,6 +11,17 @@ interface RecommendedServersProps {
 }
 
 const RecommendedServers: FC<RecommendedServersProps> = ({ server }) => {
+    const router = useRouter();
+
+    const joinServer = () => {
+        axios.post('api/join-server', { serverId: server.id })
+            .then((server) => {
+                console.log('success server join', server)
+                router.push(`/server/${server.data.id}`)
+
+            })
+            .catch((err) => console.log('server creation error', err))
+    }
     return (
         <div className='text-slate-900 my-2 w-full rounded-md px-3 py-2 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/10 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 dark:text-slate-100'>
             <div className='flex flex-col gap-y-1'>
@@ -19,18 +34,18 @@ const RecommendedServers: FC<RecommendedServersProps> = ({ server }) => {
                 <div className='flex items-center justify-between text-sm text-slate-600 dark:text-slate-400'>
                     <div className='flex gap-x-5'>
                         <div >
-                            Current: {server.membersCount}
+                            Current: {server.userIds.length}
                         </div>
                         <div >
                             Maximum: {server.maxMembers}
                         </div>
                     </div>
-                    <Link href={`/${server.id}`}>
-                        <button className='text-slate-900 rounded-md px-3 py-2 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/10 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 dark:text-slate-100'>
-                            {server.status === 'public' && 'Join'}
-                            {server.status === 'private' && 'Request to Join'}
-                        </button>
-                    </Link>
+                    {/* <Link href={`/server/${server.id}`}> */}
+                    <button onClick={joinServer} className='text-slate-900 rounded-md px-3 py-2 shadow-xl bg-slate-800 sm:rounded-xl dark:bg-slate-900/10 dark:backdrop-blur dark:ring-1 dark:ring-inset dark:ring-white/10 dark:text-slate-100'>
+                        {server.status === 'public' && 'Join'}
+                        {server.status === 'private' && 'Request to Join'}
+                    </button>
+                    {/* </Link> */}
 
 
                 </div>
