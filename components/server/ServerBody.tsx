60c11442a1d7b204ac99f307011bbbf6bf6f69ee
JstVex <heinhtetlulu14@gmail.com>
2023-06-13 23:33:39 +0630

    Create body for each channel

---
+++ components/server/ServerBody.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface ServerBodyProps {
+
+}
+
+const ServerBody: FC<ServerBodyProps> = ({ }) => {
+    return (
+        <div className='flex-grow'>
+            ServerBody
+        </div>
+    )
+}
+
+export default ServerBody
